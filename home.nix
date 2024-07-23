9e0fc5cec12f0e049006c90b3092313d550d79d4
Adi Baron <adi.baron@nanit.com>
2024-07-23 21:51:04 +0300

    initial commit

---
+++ home.nix
@@ -0,0 +1,226 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "adibaron";
+  home.homeDirectory = "/Users/adibaron";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    pkgs.zsh
+    pkgs.lsd
+    pkgs.bat
+    pkgs.kubecolor
+    pkgs.ripgrep
+    pkgs.tree
+    pkgs.xclip
+    pkgs.starship
+    pkgs.wezterm
+    pkgs.git
+    pkgs.nix-direnv
+    pkgs.clj-kondo
+    pkgs.arc-browser
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    ".config/nvim".source = ./nvim-config;
+    ".config/nvim".recursive = true;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/adibaron/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    NIXPKGS_ALLOW_UNFREE = 1;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autocd = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ls = "lsd";
+      la = "ls -la";
+      ll = "ls -l";
+      lt = "ls --tree";
+      cat = "bat";
+      v = "nvim";
+      vi = "nvim";
+      ".." = "cd ..";
+      hme = "nvim ~/.config/home-manager/home.nix";
+      hms = "home-manager switch";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      add_newline = true;
+
+      format = '' $username[](bg:#2a9d8f fg:#264653)$directory[](fg:#2a9d8f bg:#e9c46a)$git_branch$git_status[](fg:#e9c46a bg:#f4a261)$rust$scala$java$python$lua[](fg:#f4a261 bg:#e76f51)[ ](fg:#e76f51)$fill [](#e76f51)$time[](bg:#e76f51 fg:#f4a261)[](bg:#f4a261 fg:#e9c46a)[](bg:#e9c46a fg:#2a9d8f)$memory_usage[](bg:#2a9d8f fg:#264653)$nix_shell$line_break$character'';
+
+      nix_shell = {
+        disabled = false;
+        impure_msg = "⍻";
+        pure_msg = "⎷";
+        symbol = "";
+        style = "bg:#264653 fg:#ffffff";
+        format = "[ nix $state ]($style)";
+      };
+
+      memory_usage = {
+        disabled = false;
+        threshold = 0;
+        format = "[ $ram( | $swap) ]($style)";
+        style = "bg:#2a9d8f fg:#ffffff";
+      };
+
+      java = {
+        disabled = false;
+        style = "fg:#ffffff bg:#f4a261";
+        format = "[ JVM $version ]($style)";
+      };
+
+      lua = {
+        disabled = false;
+        style = "fg:#ffffff bg:#f4a261";
+        format = "[ Lua $version ]($style)";
+      };
+
+      line_break = {
+        disabled = false;
+      };
+
+      fill = {
+        symbol = "-";
+        style = "fg:#e9c46a";
+      };
+
+      character = {
+        #success_symbol = "[➜](bold #e76f51)";
+      };
+
+      username = {
+        show_always = true;
+        style_user = "bg:#264653 fg:#ffffff";
+        format = "[  $user ]($style)";
+        disabled = false;
+      };
+
+      directory = {
+        style = "bg:#2a9d8f fg:#ffffff";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncate_to_repo = false;
+        truncation_symbol = ".../";
+      };
+
+      git_branch = {
+        symbol = "";
+        style = "bg:#e9c46a fg:#000000";
+        format = "[ $symbol $branch ]($style)";
+      };
+
+      git_status = {
+        style = "bg:#e9c46a fg:#000000";
+        format = "[$all_status$ahead_behind ]($style)";
+      };
+
+      time = {
+        disabled = false;
+        style = "bg:#e76f51 fg:#ffffff";
+        format = "[  $time ]($style)";
+      };
+
+
+    };
+  };
+
+  programs.wezterm = {
+    enable = true;
+
+    extraConfig = ''
+      return {
+        color_scheme = "tokyonight_storm",
+        hide_tab_bar_if_only_one_tab = true,
+        initial_rows = 57,
+        initial_cols = 240,
+      }
+    '';
+  };
+
+  programs.neovim = {
+    enable = true;
+
+    #    extraLuaConfig = ''
+    #  ${builtins.readFile ~/.config/home-manager/nvim/init.lua}
+    #'';
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
---
+++ nvim-config/lua/vim-options.lua
@@ -0,0 +1,103 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- set to true if NerdFont is installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- show line numbers
+vim.opt.number = true
+-- enable relative line numbers
+vim.opt.relativenumber = true
+
+-- don't show the mode
+vim.opt.showmode = false
+
+-- enable mouse mode
+vim.opt.mouse = 'a'
+
+-- sync clipboard between OS and Neovim (:help :clipboard)
+vim.opt.clipboard = 'unnamedplus'
+
+-- enable break indent
+vim.opt.breakindent = true
+
+-- save undo history
+vim.opt.undofile = true
+
+-- case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- decrease update time
+vim.opt.updatetime = 250
+
+-- decrease mapped sequence wait time
+-- displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- sets how neovim will display certain whitespace characters in the editor.
+--  see `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- show which line your cursor is on
+vim.opt.cursorline = true
+
+-- minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+
+
+-- [[ Basic Keymaps ]]
+--  see `:help vim.keymap.set()`
+
+-- set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+
